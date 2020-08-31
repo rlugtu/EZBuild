61506dd0540e1ef505f7f43afcb05d555d9ab663
David Anderson <d.c.anderson@live.com>
2020-08-31 12:32:06 -0400

    Fleshed out the seeds a little. Scratch.js contains queries for niche and price range.

--- db/seeds/scratch.js
+++ db/seeds/scratch.js
@@ -1,3 +1,31 @@
-SELECT parts.cpu_id, parts.gpu_id, builds.cpu_id, builds.gpu_id
+// Each build will offer a unique combination of CPU and GPU, so we'll be using that as our standard for our select queries.
+
+// Get all gaming builds and display CPU/GPU
+SELECT parts.make, parts.model, builds.build_name
 FROM builds
-INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.cpu_id;
+INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.gpu_id AND builds.build_type='gaming';
+
+// Get all streaming builds and display CPU/GPU
+SELECT parts.make, parts.model, builds.build_name
+FROM builds
+INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.gpu_id AND builds.build_type='streaming';
+
+// Get all creation builds and display CPU/GPU
+SELECT parts.make, parts.model, builds.build_name
+FROM builds
+INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.gpu_id AND builds.build_type='creation';
+
+// Get all cheap builds
+SELECT parts.make, parts.model, builds.build_name
+FROM builds
+INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.gpu_id AND builds.price_tier='bronze';
+
+// Get all mid-range builds
+SELECT parts.make, parts.model, builds.build_name
+FROM builds
+INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.gpu_id AND builds.price_tier='silver';
+
+// Get all luxury builds
+SELECT parts.make, parts.model, builds.build_name
+FROM builds
+INNER JOIN parts ON builds.cpu_id=parts.cpu_id AND builds.gpu_id=parts.gpu_id AND builds.price_tier='gold';
