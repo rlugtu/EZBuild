494fd6b7c49a41d2a4f61a1d8c172172661054ce
David Anderson <d.c.anderson@live.com>
2020-09-01 12:34:23 -0400

    Last minute tweaks.

--- db/seeds/builds_seed.sql
+++ db/seeds/builds_seed.sql
@@ -5,14 +5,14 @@ VALUES
 INSERT INTO BUILDS
     (price_tier, build_type, build_name, build_description, cpu_id, gpu_id, ram_id)
 VALUES
-    ('silver', 'gaming', 'el leon', 'built for mid-level gaming', 2, 2, 1);
+    ('silver', 'gaming', 'el leon', 'built for mid-level gaming', 2, 2, 2);
 INSERT INTO BUILDS
     (price_tier, build_type, build_name, build_description, cpu_id, gpu_id, ram_id)
 VALUES
-    ('silver', 'streaming', 'il pinguino', 'built for mid-level streaming', 2, 2);
+    ('silver', 'streaming', 'il pinguino', 'built for mid-level streaming', 2, 2, 2);
 
 INSERT INTO BUILDS
     (price_tier, build_type, build_name, build_description, cpu_id, gpu_id, ram_id)
 VALUES
-    ('gold', 'gaming', 'le lion', 'built for intense gaming', 3, 3);
+    ('gold', 'gaming', 'le lion', 'built for intense gaming', 3, 3, 3);
 
