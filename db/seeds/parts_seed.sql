4e53ce9ba5de871b50de57a70326dc1100cd0fbe
David Anderson <d.c.anderson@live.com>
2020-09-02 10:52:55 -0400

    Some refactoring, easier to grab parts. More detail for all of our parts.

--- db/seeds/parts_seed.sql
+++ db/seeds/parts_seed.sql
@@ -22,34 +22,41 @@ VALUES (4, 140, 'bronze', 'db/assets/xfx_radeon.jpg', 'Placeholder part descript
 INSERT INTO PARTS (gpu_id, price, price_tier, image_url, part_description, make, model, boost_clock, memory, gsync, freesync)
 VALUES (5, 350, 'silver', 'db/assets/xfx_radeon.jpg', 'Placeholder part description - RTX 2070 Super', 'NVIDIA', 'GeForce RTX 2070 Super', 1605, '8 GB GDDR6', true, false);
 
+INSERT INTO PARTS  (ram_id, make, model, capacity, price, price_tier, transfer_rate)
+VALUES (1, 'Corsair', 'Vengeance', '4GB', 20, 'bronze', 2666);
 INSERT INTO PARTS  (ram_id, capacity, price, price_tier, transfer_rate)
-VALUES (1, '4GB', 20, 'bronze', 2666);
+VALUES (2, 'Kingston', 'HyperX Fury', '8GB', 40, 'bronze', 2666);
 INSERT INTO PARTS  (ram_id, capacity, price, price_tier, transfer_rate)
-VALUES (2, '8GB', 40, 'bronze', 2666);
+VALUES (3, 'Kingston', 'HyperX Predator', '16GB', 60, 'silver', 2666);
 INSERT INTO PARTS  (ram_id, capacity, price, price_tier, transfer_rate)
-VALUES (3, '16GB', 60, 'silver', 2666);
-INSERT INTO PARTS  (ram_id, capacity, price, price_tier, transfer_rate)
-VALUES (4, '32GB', 120, 'gold', 2666);
+VALUES (4, 'G.', 'Skill Trident', '32GB', 120, 'gold', 2666);
 
+INSERT INTO PARTS (ssd_id, make, capacity, price)
+VALUES (1, 'PNY' '240 GB', 30);
+INSERT INTO PARTS (ssd_id, make, capacity, price)
+VALUES (2, 'SanDisk' '512 GB', 65);
+INSERT INTO PARTS (ssd_id, make, capacity, price)
+VALUES (3, 'Western Digital', '1 TB', 150);
 INSERT INTO PARTS (ssd_id, capacity, price)
-VALUES (1, '256 GB', 50);
-INSERT INTO PARTS (ssd_id, capacity, price)
-VALUES (2, '512 GB', 75);
-INSERT INTO PARTS (ssd_id, capacity, price)
-VALUES (3, '1 TB', 100);
-INSERT INTO PARTS (ssd_id, capacity, price)
-VALUES (4, '2 TB', 150);
-
-INSERT INTO PARTS (model, price_tier, price)
-VALUES ('default cheap cooler', 'bronze', 50);
-INSERT INTO PARTS (model, price_tier, price)
-VALUES ('default mid-range cooler', 'silver', 100);
-INSERT INTO PARTS (model, price_tier, price)
-VALUES ('default luxury cooler', 'gold', 150);
+VALUES (4, 'SanDisk', '2 TB', 180);
 
 INSERT INTO PARTS (model, price_tier, price)
-VALUES ('default cheap psu', 'bronze', 50);
+VALUES ('Cooler Master Hyper 212 Evo', 'bronze', 35);
 INSERT INTO PARTS (model, price_tier, price)
-VALUES ('default mid-range psu', 'silver', 100);
+VALUES ('Corsarir H100i PRO', 'silver', 120);
 INSERT INTO PARTS (model, price_tier, price)
-VALUES ('default luxury psu', 'gold', 150);
+VALUES ('Corsair H100i PRO', 'gold', 120);
+
+INSERT INTO PARTS (model, price_tier, price)
+VALUES ('MSI Z390', 'bronze', 130);
+INSERT INTO PARTS (model, price_tier, price)
+VALUES ('ASUS Prime X570-P', 'silver', 170);
+INSERT INTO PARTS (model, price_tier, price)
+VALUES ('MSI MPG X570', 'gold', 200);
+
+INSERT INTO PARTS (model, price_tier, voltage, price)
+VALUES ('EVGA SuperNova G3', 'bronze', 550, 100);
+INSERT INTO PARTS (model, price_tier, price)
+VALUES ('EVGA SuperNova G2', 'silver', 650, 130);
+INSERT INTO PARTS (model, price_tier, price)
+VALUES ('EVGA SuperNova G2', 'gold', 650, 130);
