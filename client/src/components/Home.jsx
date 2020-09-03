dab210fe6d015c74a84312b89b860bf367012868
David Anderson <d.c.anderson@live.com>
2020-09-03 17:51:01 -0400

    First bits of styling in place.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,14 +5,18 @@ import { Link } from 'react-router-dom'
 const Home = (props) => {
     return (
         <div className="home">
-            <h1 className="brandStatement">Get the PC for your needs at the right price</h1>
+            <div className="brandStatement">
+                <h1>EZBuild</h1>
+            </div>
             <div className="categories">
                 <div className="gamersHomeButton">
                     <div className="nicheImage">
                         <Link to="/builds/gaming"><img src="https://www.nzxt.com/assets/homepage/h510-elite-white-black-dc7369e78f982928f808609aa9879b8d8e84ea9138ddbaeec97a066ab415de98.png" alt={"computer"}></img></Link>
-                        <h1>Gaming</h1>
+                        <h1>TERRESTRE</h1>
+                        <h2>Gaming</h2>
+                        <h3>Tight, lean and built for performance</h3>
+                        <p>specs seamlessly balanced for power and acuity. <br></br>balanced gpu and cpu. <br></br>all lean muscle. no fat.</p>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, molestias explicabo mollitia illum ducimus delectus officia voluptates quo incidunt dicta minus</p>
                 </div>
                 <div className="streamersHomeButton">
                     <div className="nicheImage">
